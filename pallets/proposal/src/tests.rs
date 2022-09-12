ad2daaf1cea6bb343cb805d9907adf29551b2f59
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-09-12 12:36:12 +0200

    test(proposal): Add test pattern

--- pallets/proposal/src/tests.rs
+++ pallets/proposal/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{mock::*, Error};
+use crate::{mock::*, Error, Proposal as ProposalStruct};
 use frame_support::{assert_noop, assert_ok, error::BadOrigin};
 
 pub fn last_event() -> Event {
@@ -6,9 +6,17 @@ pub fn last_event() -> Event {
 }
 
 #[test]
-fn must_be_root() {
+fn mock_works() {
 	new_test_ext().execute_with(|| {
 		// Only the `root` account can call the `register` function
 		assert_ok!(Proposal::test(Origin::signed(1)));
+	})
+}
+
+#[test]
+fn execute_proposal_works() {
+	new_test_ext().execute_with(|| {
+		let proposal = ProposalStruct { call: todo!(), metadata: todo!() };
+		assert_ok!(Proposal::add_proposal(proposal, 0xFFFFFFF));
 	})
 }
