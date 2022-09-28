ee6aee3da8c6716bac86ba2ddb01535ab0ac4729
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-09-28 15:53:17 +0200

    refactor(multi-mint): Remove unneeded `super` keyword

--- pallets/multi-mint/src/types.rs
+++ pallets/multi-mint/src/types.rs
@@ -1,31 +1,31 @@
 use frame_support::pallet_prelude::*;
 
 // Types
-pub(super) type CurrencyIdOf<T> = <T as orml_tokens::Config>::CurrencyId;
-pub(super) type AmountOf<T> = <T as orml_tokens::Config>::Amount;
-pub(super) type BalanceOf<T> = <T as orml_tokens::Config>::Balance;
+pub type CurrencyIdOf<T> = <T as orml_tokens::Config>::CurrencyId;
+pub type AmountOf<T> = <T as orml_tokens::Config>::Amount;
+pub type BalanceOf<T> = <T as orml_tokens::Config>::Balance;
 
 // Structs
 #[derive(Default, Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 pub struct CurrencyMetadata<BoundedString> {
 	/// The user friendly name of this asset. Limited in length by `StringLimit`.
-	pub(super) name: BoundedString,
+	pub name: BoundedString,
 	/// The ticker symbol for this asset. Limited in length by `StringLimit`.
-	pub(super) symbol: BoundedString,
+	pub symbol: BoundedString,
 	/// The number of decimals this asset uses to represent one unit.
-	pub(super) decimals: u8,
+	pub decimals: u8,
 }
 
 #[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 pub struct CurrencyInfo<AccountId> {
 	///
-	pub(super) current_owner: AccountId,
+	pub current_owner: AccountId,
 	///
-	pub(super) issuer: AccountId,
+	pub issuer: AccountId,
 	///
-	pub(super) transfers_enabled: TransferStatus,
+	pub transfers_enabled: TransferStatus,
 	///
-	pub(super) trading_enabled: TradingStatus,
+	pub trading_enabled: TradingStatus,
 }
 
 // Enums
