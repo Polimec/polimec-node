46d12e6d4f20bb8da5279807a55befabc70d5169
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-09-29 22:31:58 +0200

    feat(funding): Better validity check of Metadata

--- pallets/funding/src/types.rs
+++ pallets/funding/src/types.rs
@@ -1,6 +1,6 @@
-use frame_support::pallet_prelude::*;
+use frame_support::{pallet_prelude::*, BoundedVec};
 
-#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub struct ProjectMetadata<AccountId> {
 	/// The issuer of the  certificate
 	pub issuer_certifcate: Issuer,
@@ -19,24 +19,39 @@ pub struct ProjectMetadata<AccountId> {
 	/// Conversion rate of contribution token to mainnet token
 	pub conversion_rate: u32,
 	/// Participation currencies (e.g stablecoins, DOT, KSM)
-	/// TODO: Use something like Vec<Currencies>
-	pub participation_currencies: [Option<Currencies>; 4],
+	/// TODO: Use something like BoundedVec<Option<Currencies>, StringLimit>
+	/// e.g. https://github.com/paritytech/substrate/blob/427fd09bcb193c1e79dec85b1e207c718b686c35/frame/uniques/src/types.rs#L110
+	pub participation_currencies: BoundedVec<Option<Currencies>, ConstU32<4>>,
 	/// Issuer destination accounts for each accepted token (for receiving participations)
 	pub destinations_account: AccountId,
 }
+#[derive(Debug)]
+pub enum ValidityError {
+	NotEnoughParticipationCurrencies,
+	NotEnoughParticipants,
+}
 
 impl<AccountId> ProjectMetadata<AccountId> {
-	pub fn is_valid(&self) -> bool {
-		self.participation_currencies
+	pub fn validity_check(&self) -> Result<(), ValidityError> {
+		if self.minimum_participants_size == 0 {
+			return Err(ValidityError::NotEnoughParticipants)
+		}
+		if !self
+			.participation_currencies
 			.iter()
 			.any(|maybe_currency| maybe_currency.is_some())
+		{
+			return Err(ValidityError::NotEnoughParticipationCurrencies)
+		}
+
+		Ok(())
 	}
 }
 
 // Enums
 // TODO: Use SCALE fixed indexes
 // TODO: Check if it's correct
-#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub enum Issuer {
 	Kilt,
 	Other,
@@ -44,7 +59,7 @@ pub enum Issuer {
 
 // TODO: Use SCALE fixed indexes
 /// Native currency: `PLMC = [0; 8]`
-#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub enum Currencies {
 	DOT,
 	KSM,
