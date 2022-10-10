944e4666013bdf7ec99cc521b1ace22b597170e5
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-10-10 10:46:23 +0200

    feat(funding): Add evaluation status

--- pallets/funding/src/lib.rs
+++ pallets/funding/src/lib.rs
@@ -28,37 +28,43 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
+		/// Identifier for the collection of item.
+		type ProjectId: Member + Parameter + MaxEncodedLen + Copy;
+
 		/// The maximum length of data stored on-chain.
 		#[pallet::constant]
 		type StringLimit: Get<u32>;
-
-		/// The maximum number of projects issued by a single user
-		#[pallet::constant]
-		type NumberOfProjects: Get<u32>;
 	}
 
 	#[pallet::storage]
 	#[pallet::getter(fn projects_of)]
 	/// Metadata of a Project.
-	pub type ProjectsOf<T: Config> = StorageMap<
+	/// TODO: Change to DoubleStorageMap { k1: T::AccountID, k2: T::SomeProjectID, v: Project }
+	/// In this way we can remove the NumberOfProjects
+	pub type ProjectsOf<T: Config> = StorageDoubleMap<
 		_,
 		Blake2_128Concat,
 		T::AccountId,
-		BoundedVec<Project<T::AccountId, BoundedVec<u8, T::StringLimit>>, T::NumberOfProjects>,
+		Blake2_128Concat,
+		T::ProjectId,
+		(Project<T::AccountId, BoundedVec<u8, T::StringLimit>, T::BlockNumber>, EvaluationStatus),
 	>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		ProjectCreated(T::AccountId),
+		ProjectCreated(T::ProjectId, T::AccountId),
+		EvaluationStarted(T::ProjectId, T::AccountId),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		MetadataError,
-		MetadataErrorNotEnoughParticipationCurrencies,
-		MetadataErrorNotEnoughParticipants,
+		PriceTooLow,
 		TooManyProjects,
+		ParticipantsSizeError,
+		TicketSizeError,
+		ProjectIdInUse,
+		ProjectNotExists,
 	}
 
 	#[pallet::call]
@@ -66,23 +72,37 @@ pub mod pallet {
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn create(
 			origin: OriginFor<T>,
-			project: Project<T::AccountId, BoundedVec<u8, T::StringLimit>>,
+			project: Project<T::AccountId, BoundedVec<u8, T::StringLimit>, T::BlockNumber>,
+			project_id: T::ProjectId,
 		) -> DispatchResult {
 			// TODO: Ensure that the user is credentialized
 			let issuer = ensure_signed(origin)?;
 
 			match project.validity_check() {
 				Err(error) => match error {
-					ValidityError::NotEnoughParticipationCurrencies =>
-						Err(Error::<T>::MetadataErrorNotEnoughParticipationCurrencies.into()),
-					ValidityError::NotEnoughParticipants =>
-						Err(Error::<T>::MetadataErrorNotEnoughParticipants.into()),
-					ValidityError::PriceTooLow => todo!(),
-					ValidityError::ParticipantsSizeError => todo!(),
-					ValidityError::TicketSizeError => todo!(),
+					ValidityError::PriceTooLow => Err(Error::<T>::PriceTooLow.into()),
+					ValidityError::ParticipantsSizeError =>
+						Err(Error::<T>::ParticipantsSizeError.into()),
+					ValidityError::TicketSizeError => Err(Error::<T>::TicketSizeError.into()),
 				},
-				Ok(()) => Self::do_create(issuer, project),
+				Ok(()) => {
+					ensure!(
+						!ProjectsOf::<T>::contains_key(issuer.clone(), project_id),
+						Error::<T>::ProjectIdInUse
+					);
+					Self::do_create(issuer, project, project_id)
+				},
 			}
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		pub fn start_evaluation(origin: OriginFor<T>, project_id: T::ProjectId) -> DispatchResult {
+			let issuer = ensure_signed(origin)?;
+			ensure!(
+				ProjectsOf::<T>::contains_key(issuer.clone(), project_id),
+				Error::<T>::ProjectNotExists
+			);
+			Self::do_start_evaluation(issuer, project_id)
 		}
 	}
 }
@@ -92,21 +112,29 @@ use frame_support::{pallet_prelude::DispatchError, BoundedVec};
 impl<T: Config> Pallet<T> {
 	pub fn do_create(
 		who: T::AccountId,
-		project_info: Project<T::AccountId, BoundedVec<u8, T::StringLimit>>,
+		project_info: Project<T::AccountId, BoundedVec<u8, T::StringLimit>, T::BlockNumber>,
+		project_id: T::ProjectId,
 	) -> Result<(), DispatchError> {
-		if let Some(mut alredy_existing_projects) = ProjectsOf::<T>::get(&who) {
-			alredy_existing_projects
-				.try_push(project_info)
-				.map_err(|_| Error::<T>::TooManyProjects)?;
-			ProjectsOf::<T>::insert(&who, alredy_existing_projects);
-		} else {
-			// TODO: Replace 4 with the value of T::NumberOfProjects
-			let mut new_projects = BoundedVec::with_bounded_capacity(4);
-			// TODO: This `try_push` never fails
-			new_projects.try_push(project_info).map_err(|_| Error::<T>::TooManyProjects)?;
-			ProjectsOf::<T>::insert(&who, new_projects)
-		}
-		Self::deposit_event(Event::<T>::ProjectCreated(who));
+		// When a project is created the evaluation phase doesn't start automatically
+		ProjectsOf::<T>::insert(
+			who.clone(),
+			project_id,
+			(project_info, EvaluationStatus::NotYetStarted),
+		);
+		Self::deposit_event(Event::<T>::ProjectCreated(project_id, who));
 		Ok(())
+	}
+
+	pub fn do_start_evaluation(
+		who: T::AccountId,
+		project_id: T::ProjectId,
+	) -> Result<(), DispatchError> {
+		ProjectsOf::<T>::try_mutate(who.clone(), project_id, |maybe_project| {
+			let (_, evaluation_status) =
+				maybe_project.as_mut().ok_or(Error::<T>::ProjectNotExists)?;
+			*evaluation_status = EvaluationStatus::Started;
+			Self::deposit_event(Event::<T>::EvaluationStarted(project_id, who));
+			Ok(())
+		})
 	}
 }
