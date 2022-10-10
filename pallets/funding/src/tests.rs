ee181ecef9cb263e8633131889af2691470f470c
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-10-10 10:48:02 +0200

    test(funding): Add tests for create and start_evaluation

--- pallets/funding/src/tests.rs
+++ pallets/funding/src/tests.rs
@@ -1,1 +1,126 @@
+use crate::{mock::*, Error, Project};
+use frame_support::assert_ok;
 
+pub fn last_event() -> Event {
+	frame_system::Pallet::<Test>::events().pop().expect("Event expected").event
+}
+
+const ALICE: AccountId = 7;
+
+mod create {
+	use frame_support::assert_noop;
+
+	use crate::{ParticipantsSize, TicketSize};
+
+	use super::*;
+
+	#[test]
+	fn it_works() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 1,
+				ticket_size: TicketSize { minimum: Some(1), maximum: None },
+				participants_size: ParticipantsSize { minimum: Some(2), maximum: None },
+				..Default::default()
+			};
+
+			assert_ok!(FundingModule::create(Origin::signed(ALICE), project, 1));
+
+			// The event was deposited
+			assert_eq!(last_event(), Event::FundingModule(crate::Event::ProjectCreated(1, ALICE)));
+		})
+	}
+
+	#[test]
+	fn price_too_low() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 0,
+				ticket_size: TicketSize { minimum: Some(1), maximum: None },
+				participants_size: ParticipantsSize { minimum: Some(2), maximum: None },
+				..Default::default()
+			};
+
+			assert_noop!(
+				FundingModule::create(Origin::signed(ALICE), project, 1),
+				Error::<Test>::PriceTooLow
+			);
+		})
+	}
+
+	#[test]
+	fn participants_size_error() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 1,
+				ticket_size: TicketSize { minimum: Some(1), maximum: None },
+				participants_size: ParticipantsSize { minimum: None, maximum: None },
+				..Default::default()
+			};
+
+			assert_noop!(
+				FundingModule::create(Origin::signed(ALICE), project, 1),
+				Error::<Test>::ParticipantsSizeError
+			);
+		})
+	}
+
+	#[test]
+	fn ticket_size_error() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 1,
+				ticket_size: TicketSize { minimum: None, maximum: None },
+				participants_size: ParticipantsSize { minimum: Some(1), maximum: None },
+				..Default::default()
+			};
+
+			assert_noop!(
+				FundingModule::create(Origin::signed(ALICE), project, 1),
+				Error::<Test>::TicketSizeError
+			);
+		})
+	}
+
+	#[test]
+	#[ignore = "ATM only the first error will be thrown"]
+	fn multiple_field_error() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 0,
+				ticket_size: TicketSize { minimum: None, maximum: None },
+				participants_size: ParticipantsSize { minimum: None, maximum: None },
+				..Default::default()
+			};
+
+			assert_noop!(
+				FundingModule::create(Origin::signed(ALICE), project, 1),
+				Error::<Test>::TicketSizeError
+			);
+		})
+	}
+}
+
+mod evaluation {
+
+	use frame_support::assert_noop;
+
+	use crate::{EvaluationStatus, ParticipantsSize, TicketSize};
+
+	use super::*;
+	#[test]
+	fn it_works() {
+		new_test_ext().execute_with(|| {
+			let project = Project {
+				minimum_price: 1,
+				ticket_size: TicketSize { minimum: Some(1), maximum: None },
+				participants_size: ParticipantsSize { minimum: Some(2), maximum: None },
+				..Default::default()
+			};
+
+			assert_ok!(FundingModule::create(Origin::signed(ALICE), project, 1));
+			let (_, evaluation_status) = FundingModule::projects_of(ALICE, 1).unwrap();
+			assert!(evaluation_status == EvaluationStatus::NotYetStarted)
+		})
+	}
+}
