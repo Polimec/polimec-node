f6d7596f1688cffd593d21bbf72fa47f39945218
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-11-15 17:18:07 +0100

    test(credentials): Add basic tests for `add_member` and `remove_member` extrinsics

--- pallets/credentials/src/tests.rs
+++ pallets/credentials/src/tests.rs
@@ -1,5 +1,5 @@
 use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use frame_support::{assert_noop, assert_ok, error::BadOrigin};
 use polimec_traits::{Credential, MemberRole, PolimecMembers};
 
 pub fn last_event() -> RuntimeEvent {
@@ -10,30 +10,39 @@ const ALICE: AccountId = 1;
 const BOB: AccountId = 2;
 const CHARLIE: AccountId = 3;
 const DAVE: AccountId = 4;
+const EVE: AccountId = 5;
 
 #[test]
-fn genesis_works() {
+fn add_during_genesis_works() {
 	new_test_ext().execute_with(|| {
-		assert!(Credentials::members(MemberRole::Issuer).len() == 1);
+		assert!(Credentials::members(ALICE, MemberRole::Issuer).is_some());
 		assert!(Credentials::is_in(&ALICE, &MemberRole::Issuer));
 
-		assert!(Credentials::members(MemberRole::Retail).len() == 1);
+		assert!(Credentials::members(BOB, MemberRole::Retail).is_some());
 		assert!(Credentials::is_in(&BOB, &MemberRole::Retail));
 
-		assert!(Credentials::members(MemberRole::Professional).len() == 1);
+		assert!(Credentials::members(CHARLIE, MemberRole::Professional).is_some());
 		assert!(Credentials::is_in(&CHARLIE, &MemberRole::Professional));
 
-		assert!(Credentials::members(MemberRole::Institutional).len() == 1);
+		assert!(Credentials::members(DAVE, MemberRole::Institutional).is_some());
 		assert!(Credentials::is_in(&DAVE, &MemberRole::Institutional));
 	})
 }
 
 #[test]
-fn add_works() {
+fn add_member_works() {
 	new_test_ext().execute_with(|| {
 		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
-		assert_ok!(Credentials::add_member(RuntimeOrigin::signed(ALICE), BOB, cred));
+		assert_ok!(Credentials::add_member(RuntimeOrigin::root(), BOB, cred));
 		assert_eq!(last_event(), RuntimeEvent::Credentials(crate::Event::MemberAdded));
+	})
+}
+
+#[test]
+fn only_root_can_add_member() {
+	new_test_ext().execute_with(|| {
+		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
+		assert_noop!(Credentials::add_member(RuntimeOrigin::signed(ALICE), BOB, cred), BadOrigin);
 	})
 }
 
@@ -42,8 +51,39 @@ fn cant_add_already_member() {
 	new_test_ext().execute_with(|| {
 		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
 		assert_noop!(
-			Credentials::add_member(RuntimeOrigin::signed(ALICE), ALICE, cred),
+			Credentials::add_member(RuntimeOrigin::root(), ALICE, cred),
 			Error::<Test>::AlreadyMember
+		);
+	})
+}
+
+#[test]
+fn remove_member_works() {
+	new_test_ext().execute_with(|| {
+		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
+		assert_ok!(Credentials::remove_member(RuntimeOrigin::root(), ALICE, cred));
+		assert_eq!(last_event(), RuntimeEvent::Credentials(crate::Event::MemberRemoved));
+	})
+}
+
+#[test]
+fn only_root_can_remove_member() {
+	new_test_ext().execute_with(|| {
+		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
+		assert_noop!(
+			Credentials::remove_member(RuntimeOrigin::signed(ALICE), ALICE, cred),
+			BadOrigin
+		);
+	})
+}
+
+#[test]
+fn cant_remove_not_a_member() {
+	new_test_ext().execute_with(|| {
+		let cred = Credential { role: MemberRole::Issuer, ..Default::default() };
+		assert_noop!(
+			Credentials::remove_member(RuntimeOrigin::root(), EVE, cred),
+			Error::<Test>::NotMember
 		);
 	})
 }
