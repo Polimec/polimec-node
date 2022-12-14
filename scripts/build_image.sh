88f386d243f16f161dec7ba7b249a6c2cda1227e
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-12-14 15:49:52 +0100

    build: Update zombienet configuration

---
+++ scripts/build_image.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Build Docker Image from Dockerfile/Containerfile
+# Usage: ./build_image.sh <version> <path to Dockerfile/Containerfile> <package>
+# Example: ./build_image.sh latest ./Containerfile polimec-standalone-node
+
+# Check if version is passed
+if [ -z "$1" ]
+then
+    echo "Please pass version as first argument"
+    echo "Example: ./build_image.sh latest ../Containerfile polimec-standalone-node"
+    exit 1
+fi
+
+# Check if Containerfile is passed
+if [ -z "$2" ]
+then
+    echo "Please pass path to Containerfile as second argument"
+    echo "Example: ./build_image.sh latest ../Containerfile polimec-standalone-node"
+    exit 1
+fi
+
+# Check if Containerfile exists
+if [ ! -f "$2" ]
+then
+    echo "Containerfile does not exist"
+    exit 1
+fi
+
+# Check if package is passed
+if [ -z "$3" ]
+then
+    echo "Please pass package as third argument"
+    echo "Example: ./build_image.sh latest ../Containerfile polimec-standalone-node"
+    exit 1
+fi
+
+# Build Docker Image
+podman build -t "docker.io/polimec/$3:$1" -f $2 --build-arg PACKAGE=$3 ..
+exit 0
