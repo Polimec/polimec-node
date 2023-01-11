f1c265dd153a68aeb4586754fd5e56de5131317f
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2023-01-11 15:25:07 +0100

    test(funding): Benchmarkthe new note_image extrinsic

--- pallets/funding/src/benchmarking.rs
+++ pallets/funding/src/benchmarking.rs
@@ -8,22 +8,37 @@ use super::*;
 use frame_benchmarking::{account, benchmarks};
 use frame_system::{Pallet as System, RawOrigin as SystemOrigin};
 
+const METADATA: &str = r#"
+{
+	"whitepaper":"ipfs_url",
+	"team_description":"ipfs_url",
+	"tokenomics":"ipfs_url",
+	"roadmap":"ipfs_url",
+	"usage_of_founds":"ipfs_url"
+}
+"#;
+
 #[allow(unused)]
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
+#[allow(unused)]
+fn get_events<T: Config>() -> frame_benchmarking::Vec<<T as frame_system::Config>::RuntimeEvent> {
+	frame_system::Pallet::<T>::events()
+		.into_iter()
+		.map(|r| r.event)
+		.collect::<Vec<_>>()
+}
+
 fn create_default_project<T: Config>(
 	id: Option<u32>,
-) -> (
-	T::ProjectIdentifier,
-	T::AccountId,
-	Project<T::AccountId, BoundedVec<u8, T::StringLimit>, BalanceOf<T>>,
-) {
+) -> (T::ProjectIdentifier, T::AccountId, ProjectOf<T>) {
+	let issuer: T::AccountId = account::<T::AccountId>("Alice", 1, 1);
 	let project_id_parameter = id.unwrap_or(0);
 	let project_id = T::BenchmarkHelper::create_project_id_parameter(project_id_parameter);
-	let issuer: T::AccountId = account::<T::AccountId>("Alice", 1, 1);
-	let project = T::BenchmarkHelper::create_dummy_project(issuer.clone());
+	let metadata_hash = store_and_return_metadata_hash::<T>();
+	let project = T::BenchmarkHelper::create_dummy_project(issuer.clone(), metadata_hash);
 	(project_id, issuer, project)
 }
 
@@ -37,7 +52,24 @@ fn create_default_minted_project<T: Config>(
 	(project_id, issuer)
 }
 
+fn store_and_return_metadata_hash<T: Config>() -> T::Hash {
+	let issuer: T::AccountId = account::<T::AccountId>("Alice", 1, 1);
+	assert!(PolimecFunding::<T>::note_image(
+		SystemOrigin::Signed(issuer.clone()).into(),
+		METADATA.into(),
+	)
+	.is_ok());
+	// TODO: Get the hash from the Noted event
+	T::Hashing::hash(METADATA.as_bytes())
+}
+
 benchmarks! {
+	note_image {
+		let issuer: T::AccountId = account::<T::AccountId>("Alice", 1, 1);
+	}: _(SystemOrigin::Signed(issuer), METADATA.into())
+	verify {
+	}
+
 	create {
 		let (_, issuer, project) = create_default_project::<T>(None);
 	}: _(SystemOrigin::Signed(issuer), project)
