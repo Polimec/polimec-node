decdcf8561ab4d6d4c2d00fcf56b5767d8e344a5
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2023-02-27 18:21:32 +0100

    chore(runtime-standalone): Lower the Remove Items Limit

--- runtimes/standalone/src/lib.rs
+++ runtimes/standalone/src/lib.rs
@@ -336,7 +336,7 @@ impl pallet_assets::Config for Runtime {
 	type WeightInfo = ();
 	type CallbackHandle = ();
 	type AssetAccountDeposit = AssetAccountDeposit;
-	type RemoveItemsLimit = frame_support::traits::ConstU32<1000>;
+	type RemoveItemsLimit = frame_support::traits::ConstU32<512>;
 	#[cfg(feature = "runtime-benchmarks")]
 	type BenchmarkHelper = ();
 }
