08224f204385654d302548cde5fd69cbddff327d
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2023-05-01 18:05:35 +0200

    feat: remove draft credentials api

--- nodes/parachain/src/rpc.rs
+++ nodes/parachain/src/rpc.rs
@@ -54,13 +54,11 @@ where
 		+ Send
 		+ Sync
 		+ 'static,
-	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
-	C::Api: pallet_credentials_api::CredentialsRuntimeApi<Block, AccountId>,
+	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Nonce>,
 	C::Api: BlockBuilder<Block>,
 	P: TransactionPool + Sync + Send + 'static,
 {
-	use pallet_credentials_api::{Credentials, CredentialsApiServer};
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
 	use substrate_frame_rpc_system::{System, SystemApiServer};
 
@@ -68,8 +66,6 @@ where
 	let FullDeps { client, pool, deny_unsafe } = deps;
 
 	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
-	module.merge(TransactionPayment::new(client.clone()).into_rpc())?;
-	module.merge(Credentials::new(client).into_rpc())?;
-
+	module.merge(TransactionPayment::new(client).into_rpc())?;
 	Ok(module)
 }
