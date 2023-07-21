69ff08e1780d0b6b6ee35df83dadaa77a8590140
Juan Ignacio RIos <juani.rios.99@gmail.com>
2023-07-21 12:04:38 +0200

    feat(220): do_evaluation_end uses the config type as threshold. Config type renamed

--- polimec-skeleton/pallets/funding/src/lib.rs
+++ polimec-skeleton/pallets/funding/src/lib.rs
@@ -381,7 +381,7 @@ pub mod pallet {
 
 		type FeeBrackets: Get<Vec<(Percent, Self::Balance)>>;
 
-		type EarlyEvaluationThreshold: Get<Percent>;
+		type EvaluationSuccessThreshold: Get<Percent>;
 	}
 
 	#[pallet::storage]
