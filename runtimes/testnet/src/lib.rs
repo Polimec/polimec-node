8451e9c4db470c6c1adad315fe4976d4b27cca18
Juan Ignacio Rios <juani.rios.99@gmail.com>
2023-11-24 18:04:37 +0100

    working?

--- runtimes/testnet/src/lib.rs
+++ runtimes/testnet/src/lib.rs
@@ -68,7 +68,7 @@ pub use crate::xcm_config::*;
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 // Polimec Shared Imports
-use pallet_funding::{BondTypeOf, DaysToBlocks};
+use pallet_funding::{AcceptedFundingAsset, BondTypeOf, DaysToBlocks};
 #[cfg(feature = "runtime-benchmarks")]
 use pallet_funding::traits::SetPrices;
 pub use pallet_parachain_staking;
@@ -149,39 +149,35 @@ pub fn native_version() -> NativeVersion {
 }
 
 #[cfg(feature = "runtime-benchmarks")]
-struct SetOraclePrices;
+pub struct SetOraclePrices;
 #[cfg(feature = "runtime-benchmarks")]
 impl SetPrices for SetOraclePrices {
 	fn set_prices() {
-		fn values(
-			values: [f64; 4],
-		) -> BoundedVec<(u32, FixedU128), <Runtime as orml_oracle::Config<orml_oracle::Instance1>>::MaxFeedValues> {
-			let [dot, usdc, usdt, plmc] = values;
-			vec![
-				(0u32, FixedU128::from_float(dot)),
-				(420u32, FixedU128::from_float(usdc)),
-				(1984u32, FixedU128::from_float(usdt)),
-				(2069u32, FixedU128::from_float(plmc)),
-			].try_into().expect("only run in benchmarks mode, so it can panic");
-		}
 
+		let dot = (AcceptedFundingAsset::DOT.to_statemint_id(), FixedU128::from_rational(69, 1));
+		let usdc = (AcceptedFundingAsset::USDT.to_statemint_id(), FixedU128::from_rational(1, 1));
+		let usdt = (AcceptedFundingAsset::USDT.to_statemint_id(), FixedU128::from_rational(1, 1));
+		let plmc = (pallet_funding::PLMC_STATEMINT_ID, FixedU128::from_rational(840, 100));
+
+		let values: BoundedVec<(u32, FixedU128), <Runtime as orml_oracle::Config<orml_oracle::Instance1>>::MaxFeedValues> = vec![
+			dot, usdc, usdt, plmc].try_into().expect("benchmarks can panic");
 		let alice: [u8; 32] = [212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125];
 		let bob: [u8; 32] = [142, 175, 4, 21, 22, 135, 115, 99, 38, 201, 254, 161, 126, 37, 252, 82, 135, 97, 54, 147, 201, 18, 144, 156, 178, 38, 170, 71, 148, 242, 106, 72];
 		let charlie: [u8; 32] = [144, 181, 171, 32, 92, 105, 116, 201, 234, 132, 27, 230, 136, 134, 70, 51, 220, 156, 168, 163, 87, 132, 62, 234, 207, 35, 20, 100, 153, 101, 254, 34];
 
 		frame_support::assert_ok!(Oracle::feed_values(
 			RuntimeOrigin::signed(alice.clone().into()),
-			values([4.84, 1.0, 1.0, 0.4])
+			values.clone()
 		));
 
 		frame_support::assert_ok!(Oracle::feed_values(
 			RuntimeOrigin::signed(bob.clone().into()),
-			values([4.84, 1.0, 1.0, 0.4])
+			values.clone()
 		));
 
 		frame_support::assert_ok!(Oracle::feed_values(
 			RuntimeOrigin::signed(charlie.clone().into()),
-			values([4.84, 1.0, 1.0, 0.4])
+			values.clone()
 		));
 	}
 }
