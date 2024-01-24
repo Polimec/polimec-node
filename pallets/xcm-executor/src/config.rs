f7d83e09d58dd6a085c60344dc0540cc38aecf5b
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-01-24 11:08:12 +0100

    chore: fix more header

--- pallets/xcm-executor/src/config.rs
+++ pallets/xcm-executor/src/config.rs
@@ -1,21 +1,20 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+
 // Polimec Blockchain – https://www.polimec.org/
 // Copyright (C) Polimec 2022. All rights reserved.
 
-// Copyright (C) Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
-
-// Polkadot is free software: you can redistribute it and/or modify
+// The Polimec Blockchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Polkadot is distributed in the hope that it will be useful,
+// The Polimec Blockchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::polimec_traits::HrmpHandler;
 use frame_support::{
