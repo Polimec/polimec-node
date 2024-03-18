7fc80fef65ae33a598fb9107dd2523a4814d497d
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-03-18 12:47:56 +0100

    chore: bump spec version

--- runtimes/base/src/lib.rs
+++ runtimes/base/src/lib.rs
@@ -195,7 +195,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polimec-mainnet"),
 	impl_name: create_runtime_str!("polimec-mainnet"),
 	authoring_version: 1,
-	spec_version: 0_005_005,
+	spec_version: 0_005_006,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
