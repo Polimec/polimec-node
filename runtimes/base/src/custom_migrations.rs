1378453087805ea6b96322bd9cf671d64f6774cd
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-03-19 11:19:32 +0100

    feat: add vesting migration

--- runtimes/base/src/custom_migrations.rs
+++ runtimes/base/src/custom_migrations.rs
@@ -18,13 +18,17 @@
 use crate::*;
 
 // Substrate
-#[allow(unused_imports)]
 use frame_support::{
 	dispatch::DispatchError,
-	log, migration,
+	log,
 	storage::unhashed,
-	traits::{GetStorageVersion, PalletInfoAccess, StorageVersion},
+	traits::{tokens::Precision::Exact, GetStorageVersion, PalletInfoAccess, StorageVersion},
 };
+use pallet_vesting::Vesting;
+use sp_runtime::AccountId32;
+
+#[cfg(feature = "try-runtime")]
+use sp_core::crypto::Ss58Codec;
 
 pub struct InitializePallet<Pallet: GetStorageVersion<CurrentStorageVersion = StorageVersion> + PalletInfoAccess>(
 	sp_std::marker::PhantomData<Pallet>,
@@ -50,4 +54,92 @@ impl<Pallet: GetStorageVersion<CurrentStorageVersion = StorageVersion> + PalletI
 		}
 		<Runtime as frame_system::Config>::DbWeight::get().reads_writes(1, 1)
 	}
+}
+
+// The `VestingInfo` fields from `pallet_vesting` are private, so we need to define them here.
+#[derive(parity_scale_codec::Encode, parity_scale_codec::Decode, sp_runtime::RuntimeDebug, Eq, PartialEq)]
+struct VestingInfo {
+	locked: Balance,
+	per_block: Balance,
+	starting_block: BlockNumber,
+}
+
+const ENCODED_STORAGE_KEY: &str =
+	"0x5f27b51b5ec208ee9cb25b55d87282435f27b51b5ec208ee9cb25b55d872824334f5503ce555ea3ee18396f4bde1b40bc28dbf096b5acf3c0d87dd8ef8cabea0794cc72200a2368751a0fe470d5f9f69";
+
+pub struct UnhashedMigration;
+impl frame_support::traits::OnRuntimeUpgrade for UnhashedMigration {
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::DispatchError> {
+		log::info!("Pre-upgrade");
+		check_balances();
+		Ok(Vec::new())
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade(_state: Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
+		log::info!("Post-upgrade");
+		check_balances();
+		let total_issuance = Balances::total_issuance();
+		assert_eq!(total_issuance, 100_000_000 * PLMC);
+		Ok(())
+	}
+
+	fn on_runtime_upgrade() -> frame_support::weights::Weight {
+		// This account received a wrong vesting schedule.
+		let acct: AccountId32 =
+			hex_literal::hex!["c28dbf096b5acf3c0d87dd8ef8cabea0794cc72200a2368751a0fe470d5f9f69"].into();
+
+		if let Ok(k) = array_bytes::hex2bytes(ENCODED_STORAGE_KEY) {
+			// If `is_some` which means it hasn't been migrated yet.
+			// But actually, without this correction, the account migration will fail.
+			// +1 R
+			if let Some(value) = unhashed::get::<Vec<VestingInfo>>(&k) {
+				let v = vec![
+					VestingInfo { locked: 119574300000000, per_block: 182000456, starting_block: 249000 },
+					VestingInfo { locked: 6485400000000, per_block: 9870000, starting_block: 249000 },
+				];
+				if value != v {
+					log::info!("⚠️ Correcting storage for {:?}", acct.encode());
+					// +1 W
+					unhashed::put::<Vec<VestingInfo>>(&k, &v);
+				} else {
+					log::info!("✅ Storage for {:?} is already correct", acct.encode());
+				}
+			}
+		}
+
+		// +1 R
+		let total_issuance = Balances::total_issuance();
+
+		if total_issuance != 100_000_000 * PLMC {
+			log::info!("⚠️ Correcting total issuance from {} to 100_000_000", total_issuance);
+			// +1 R
+			let treasury_account = PayMaster::get();
+			// +1 W
+			let _ = <Balances as tokens::fungible::Balanced<AccountId>>::deposit(
+				&treasury_account,
+				39988334 + 70094167,
+				Exact,
+			);
+		}
+
+		<Runtime as frame_system::Config>::DbWeight::get().reads_writes(3, 2)
+	}
+}
+
+#[cfg(feature = "try-runtime")]
+fn check_balances() {
+	let acct: AccountId32 =
+		hex_literal::hex!["c28dbf096b5acf3c0d87dd8ef8cabea0794cc72200a2368751a0fe470d5f9f69"].into();
+	let balance = Balances::balance(&acct);
+	log::info!("Account: {} | Balance: {}", acct.to_ss58check(), balance);
+	let vesting_stored = <Vesting<Runtime>>::get(acct.clone());
+	if let Some(vesting) = vesting_stored {
+		log::info!("Vesting: {:?}", vesting);
+	} else {
+		log::info!("Vesting: None");
+	}
+	let total_issuance = Balances::total_issuance();
+	log::info!("Total Issuance: {}", total_issuance);
 }
