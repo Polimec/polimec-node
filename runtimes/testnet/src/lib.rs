ab9ce816275c345a8358425f5fdf1c59cd2c58d5
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2024-03-20 13:28:25 +0100

    fix: testnet benchmarks

--- runtimes/testnet/src/lib.rs
+++ runtimes/testnet/src/lib.rs
@@ -24,6 +24,7 @@ extern crate frame_benchmarking;
 use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
 use cumulus_primitives_core::{AggregateMessageOrigin, ParaId};
 use frame_support::{
+	genesis_builder_helper::{build_config, create_default_config},
 	construct_runtime, ord_parameter_types, parameter_types,
 	traits::{
 		fungible::{Credit, HoldConsideration, Inspect},
@@ -1238,6 +1239,15 @@ impl_runtime_apis! {
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
+		}
+	}
+	impl sp_genesis_builder::GenesisBuilder<Block> for Runtime {
+		fn create_default_config() -> Vec<u8> {
+			create_default_config::<RuntimeGenesisConfig>()
+		}
+
+		fn build_config(config: Vec<u8>) -> sp_genesis_builder::Result {
+			build_config::<RuntimeGenesisConfig>(config)
 		}
 	}
 }
