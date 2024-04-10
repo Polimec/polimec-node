e6b4f55b92d3dc0232dc4fb836ff287c59d2c555
Just van Stam <just.van.stam@gmail.com>
2024-04-10 09:45:44 +0200

    fmt

--- pallets/funding/src/migration.rs
+++ pallets/funding/src/migration.rs
@@ -1,7 +1,5 @@
 //! A module that is responsible for migration of storage.
-use frame_support::{
-	traits::{StorageVersion},
-};
+use frame_support::traits::StorageVersion;
 
 /// The current storage version
 pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
