455e00e21cbd34ebb803b80ed53ba29c34e33a0e
Juan Ignacio Rios <54085674+JuaniRios@users.noreply.github.com>
2024-04-12 15:25:34 +0200

    Fix/Identity benchmarks (#231)
    * fmt
    
    * chore: cleanup comments
    
    * chore: move types to shared config
    
    * docs: fix comment
    
    * chore: move origin to shared config
    
    * fix: remove double MaxSubAccounts
    
    ---------
    
    Co-authored-by: Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>

--- runtimes/polimec/src/lib.rs
+++ runtimes/polimec/src/lib.rs
@@ -32,7 +32,7 @@ use frame_support::{
 	},
 	weights::{ConstantMultiplier, Weight},
 };
-use frame_system::{EnsureNever, EnsureRoot, EnsureRootWithSuccess, EnsureSigned};
+use frame_system::{EnsureRoot, EnsureRootWithSuccess, EnsureSigned};
 use pallet_democracy::GetElectorate;
 
 use parachains_common::{
@@ -892,7 +892,7 @@ impl pallet_identity::Config for Runtime {
 	type SigningPublicKey = <Signature as Verify>::Signer;
 	type Slashed = Treasury;
 	type SubAccountDeposit = SubAccountDeposit;
-	type UsernameAuthorityOrigin = EnsureNever<AccountId>;
+	type UsernameAuthorityOrigin = UsernameAuthorityOrigin;
 	type WeightInfo = pallet_identity::weights::SubstrateWeight<Runtime>;
 }
 
