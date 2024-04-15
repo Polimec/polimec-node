49ffe93dd31dd784c2c0ac8e61678383fc223fa7
Juan Ignacio Rios <juani.rios.99@gmail.com>
2024-04-15 14:04:47 +0200

    :rocket: increase spec version.  fix bug

--- runtimes/politest/src/lib.rs
+++ runtimes/politest/src/lib.rs
@@ -215,7 +215,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("politest"),
 	impl_name: create_runtime_str!("politest"),
 	authoring_version: 1,
-	spec_version: 0_006_004,
+	spec_version: 0_006_005,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
