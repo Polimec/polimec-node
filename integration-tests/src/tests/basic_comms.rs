fafa96920b73e6e0a41a9fd5ff726aa607453943
Juan Ignacio Rios <juani.rios.99@gmail.com>
2024-04-17 15:59:10 +0200

    :technologist: better dbg

--- integration-tests/src/tests/basic_comms.rs
+++ integration-tests/src/tests/basic_comms.rs
@@ -152,16 +152,15 @@ fn sandbox() {
 	let max_contributions_per_user: u32 = <PolitestRuntime as pallet_funding::Config>::MaxContributionsPerUser::get();
 	let max_insertion_attempts: u32 = <PolitestRuntime as pallet_funding::Config>::MaxProjectsToUpdateInsertionAttempts::get();
 	let block_weights = <PolitestRuntime as frame_system::Config>::BlockWeights::get();
-
-	let weight_charged = pallet_funding::WeightInfoOf::<PolitestRuntime>::contribution( max_contributions_per_user - 1)
-		.max(pallet_funding::WeightInfoOf::<PolitestRuntime>::contribution_ends_round(
+	let contribute = pallet_funding::WeightInfoOf::<PolitestRuntime>::contribution( max_contributions_per_user - 1);
+	let contribute_ends_round = pallet_funding::WeightInfoOf::<PolitestRuntime>::contribution_ends_round(
 			// Last contribution possible before having to remove an old lower one
 			max_contributions_per_user - 1,
 			// Since we didn't remove any previous lower contribution, we can buy all remaining CTs and try to move to the next phase
 			max_insertion_attempts - 1,
-		));
+		);
 
 	dbg!(&block_weights.per_class);
-	dbg!(weight_charged);
-	//
+	dbg!(contribute);
+	dbg!(contribute_ends_round);
 }
