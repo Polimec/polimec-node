4319788863a4e87feb55d53b3959a86c0cf4dfc5
Just van Stam <just.van.stam@gmail.com>
2024-04-30 12:59:06 +0200

    bump spec_version

--- runtimes/polimec/src/lib.rs
+++ runtimes/polimec/src/lib.rs
@@ -205,7 +205,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polimec-mainnet"),
 	impl_name: create_runtime_str!("polimec-mainnet"),
 	authoring_version: 1,
-	spec_version: 0_006_001,
+	spec_version: 0_007_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
