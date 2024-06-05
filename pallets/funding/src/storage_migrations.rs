40a88494d8a297880ba8f894aff39adfcbbe0d0a
Juan Ignacio Rios <juani.rios.99@gmail.com>
2024-06-05 11:43:28 +0200

    fix storage migration

--- pallets/funding/src/storage_migrations.rs
+++ pallets/funding/src/storage_migrations.rs
@@ -170,6 +170,23 @@ pub mod v3 {
 	use sp_core::{Decode, Encode, MaxEncodedLen, RuntimeDebug};
 	use sp_std::marker::PhantomData;
 
+	#[derive(Default, Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+	pub enum OldProjectStatus {
+		#[default]
+		Application,
+		EvaluationRound,
+		AuctionInitializePeriod,
+		AuctionOpening,
+		AuctionClosing,
+		CommunityRound,
+		RemainderRound,
+		FundingFailed,
+		AwaitingProjectDecision,
+		FundingSuccessful,
+		ReadyToStartMigration,
+		MigrationCompleted,
+	}
 	#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, MaxEncodedLen, TypeInfo)]
 	pub struct OldProjectDetails<
 		AccountId,
@@ -186,7 +203,7 @@ pub mod v3 {
 		/// The price in USD per token decided after the Auction Round
 		pub weighted_average_price: Option<Price>,
 		/// The current status of the project
-		pub status: ProjectStatus,
+		pub status: OldProjectStatus,
 		/// When the different project phases start and end
 		pub phase_transition_points: PhaseTransitionPoints<BlockNumber>,
 		/// Fundraising target amount in USD (6 decimals)
@@ -215,12 +232,26 @@ pub mod v3 {
 			let mut items = 0;
 			let mut translate = |_key, item: OldProjectDetailsOf<T>| -> Option<ProjectDetailsOf<T>> {
 				items += 1;
+				let new_status = match item.status {
+					OldProjectStatus::Application => ProjectStatus::Application,
+					OldProjectStatus::EvaluationRound => ProjectStatus::EvaluationRound,
+					OldProjectStatus::AuctionInitializePeriod => ProjectStatus::AuctionInitializePeriod,
+					OldProjectStatus::AuctionOpening => ProjectStatus::AuctionOpening,
+					OldProjectStatus::AuctionClosing => ProjectStatus::AuctionClosing,
+					OldProjectStatus::CommunityRound => ProjectStatus::CommunityRound,
+					OldProjectStatus::RemainderRound => ProjectStatus::RemainderRound,
+					OldProjectStatus::FundingFailed => ProjectStatus::FundingFailed,
+					OldProjectStatus::AwaitingProjectDecision => ProjectStatus::AwaitingProjectDecision,
+					OldProjectStatus::FundingSuccessful => ProjectStatus::FundingSuccessful,
+					OldProjectStatus::ReadyToStartMigration => ProjectStatus::ReadyToStartMigration,
+					OldProjectStatus::MigrationCompleted => ProjectStatus::MigrationCompleted,
+				};
 				Some(ProjectDetailsOf::<T> {
 					issuer_account: item.issuer_account,
 					issuer_did: item.issuer_did,
 					is_frozen: item.is_frozen,
 					weighted_average_price: item.weighted_average_price,
-					status: item.status,
+					status: new_status,
 					phase_transition_points: item.phase_transition_points,
 					fundraising_target_usd: item.fundraising_target_usd,
 					remaining_contribution_tokens: item.remaining_contribution_tokens,
